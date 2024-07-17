673147a78ae1885a71844a62e899604fc8aae97d
Leonid Isakov <64974600+Peygy@users.noreply.github.com>
2024-07-18 02:16:49 +0300

    Complete make googleddrive microservice

--- MainApp/GoogleDriveApp/Program.cs
+++ MainApp/GoogleDriveApp/Program.cs
@@ -6,14 +6,18 @@ builder.Services.AddStackExchangeRedisCache(option =>
     option.Configuration = builder.Configuration["RedisConnection"];
 });
 
-builder.Services.AddGrpc();
+builder.Services.AddGrpc(option =>
+{
+    option.MaxSendMessageSize = 16 * 1024 * 1024;
+    option.MaxReceiveMessageSize = 16 * 1024 * 1024;
+});
 
 builder.Services.AddSingleton<ITracksCachingService, TracksCachingService>();
 builder.Services.AddSingleton<IGoogleDriveApi, GoogleDriveApi>();
 
 var app = builder.Build();
 
-app.MapGrpcService<FileDownloaderService>();
+app.MapGrpcService<GoogleDriveAppConnectorService>();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- MainApp/MainApp/Controllers/Api/ApiDriveController.cs
+++ MainApp/MainApp/Controllers/Api/ApiDriveController.cs
@@ -1,7 +1,4 @@
-﻿using Google.Protobuf;
-using Grpc.Net.Client;
-using MainApp.Protos;
-using MainApp.Services;
+﻿using MainApp.Services.Music;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MainApp.Controllers.Api
@@ -10,27 +7,17 @@ namespace MainApp.Controllers.Api
     [ApiController]
     public class ApiDriveController : ControllerBase
     {
-        //private readonly IGoogleDriveApi driveApi;
+        private readonly IGoogleDriveAppConnectorService driveAppConnectorService;
 
-        public ApiDriveController(/*IGoogleDriveApi driveApi*/)
+        public ApiDriveController(IGoogleDriveAppConnectorService driveAppConnectorService)
         {
-            //this.driveApi = driveApi;
+            this.driveAppConnectorService = driveAppConnectorService;
         }
 
         [HttpGet("download/file")]
         public async Task<IActionResult> DownloadFile(string fileId)
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:7241", new GrpcChannelOptions
-            {
-                MaxReceiveMessageSize = 10 * 1024 * 1024
-            });
-            var client = new FileDownloader.FileDownloaderClient(channel);
-
-            var downloadRequest = new DownloadRequest { FileId = fileId };
-            var downloadResponse = await client.DownloadFileStreamAsync(downloadRequest);
-            var byteArray = downloadResponse.FileData.ToByteArray();
-
-            var fileStream = /*await driveApi.DownloadFile(fileId)*/new MemoryStream(byteArray);
+            var fileStream = await driveAppConnectorService.DownloadFile(fileId);
             if (fileStream == null)
             {
                 return NotFound();
